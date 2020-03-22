563318a616cf41b58f82e47efd50331aae99a3d3
Alouiscious Media <info@reneesoul.com>
2020-03-22 02:00:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+  return (string="special") => {
+    return `You are ${flair}${string}${flair}!`
+  }
+}
+
+let Calculator = {
+  add(x, y)  { 
+    return x + y
+  },
+  subtract(x, y) {
+    return x - y
+  },
+  multiply(x, y) {
+    return x * y
+  },
+  divide(x, y) {
+    return x / y
+  }
+} 
+
+function actionApplyer(int, arr) {
+  let x = int
+  for (let i=0; i < arr.length; i++) {
+    x = arr[i](x)
+  }
+  
+  return x
+
+};
